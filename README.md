7a38174c2be348429273c301cc3dd94e7b83fe2b
garysan <gary.gsv@gmail.com>
2018-03-12 22:47:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Normativa de ASFI | Offline
+
+## Que es?
+Este es un script escrito en Python para obtener la normativa de la ASFI y poder distribuirlo por red.
+
+## Requerimientos
+- Apache o WAMP
+- Paquetes mechanize y subprocess (disponibles en cualquier distribución)
+- Python 2.6 o Superior
+- Paquetes zip y unzip (Para el proceso de extraccion)
+- git
+
+## Modo de uso
+- Crear la estructura 
+	mkdir /home/reconorm/
+	chmod 760 /home/reconorm/
+	mkdir /home/scripts/
+	cd /home/scripts/
+
+- Debe sincronizar el repositorio usando git:
+	git clone https://github.com/garysan/normativaOffline.git
+- Correr el script
+	python circular.py
+
+## Contacto 
+Enviame un correo a gary.gsv@gmail.com
+
+## Errores
+Notificalos por GITHUB.
+
---
+++ circular.py
@@ -0,0 +1,68 @@
+#################################################
+# Script  : circular.py
+# Autor   : Ing. Gary Sandi Vigabriel
+# Objetivo: Obtencion de circulares de la ASFI
+#################################################
+
+import mechanize
+import subprocess
+
+link_asfi="TEMPO"
+
+def get_circular(parametro):
+	url_circular = 'http://servdmzw.asfi.gob.bo/GeneraActualizacionCirculares/Paginas/archivo.aspx?Numeros='+parametro
+	brc = mechanize.Browser()
+	rcirc = brc.open(url_circular)
+	tcirc= rcirc.read()
+	lseg=tcirc.split("</a>")
+	lseg2=lseg[0].split(">")
+	link=lseg2[1]
+	return_code = subprocess.call(["wget","-c",link])
+	global link_asfi
+	link_asfi=lseg2[1]
+	print return_code
+
+def circular_online():
+	url = 'http://servdmzw.asfi.gob.bo/GeneraActualizacionCirculares/Paginas/Actualizaciones.aspx'
+	br = mechanize.Browser()
+	response = br.open(url)
+	valores= response.read()
+	seg=valores.split("</label>")
+	cir=seg[0].replace("<label id='Circulares'>","")
+	circular=cir.split(",")
+	return circular[-1]
+
+def circular_local():
+	command = "ls -a /var/www/html/asfi/Circulares/ASFI_*"
+	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	output, error = process.communicate()
+	archivos = output.split("\n")
+	lastfile = archivos[-2].split(".pdf")
+	localfile= lastfile[0].split("/")
+	return localfile[6]
+
+def uncompress(link):
+	larch=link.split("/")
+	arch=larch[-1]	
+	ucode= subprocess.call(["unzip","-o",arch,"-d","/home/"])
+
+
+local=circular_local()
+print local
+
+online=circular_online()
+print online
+
+if local==online:
+	print "No tengo circulares Nuevas"
+
+else:
+	lfon1=local.split("_")
+	lfon=lfon1[1]
+	onfi1=online.split("_")
+	onfi=onfi1[1]
+	for circular in range(int(lfon)+1,int(onfi)+1):
+		cirasfi="ASFI_"+str(circular)
+		print "Obteniendo... "+cirasfi
+		get_circular(cirasfi)
+		uncompress (link_asfi)
